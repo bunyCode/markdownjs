0734aa5ab51f0516fea27aa733e0ede5db41b225
Daniel fernando reyes v <dfreyes1236@gmail.com>
2023-11-28 12:12:24 -0500

    Add most features

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const regexBlockquote = /\>(.+)/g;
+const regexImg = /\!\[(.+)\]\((.+)\)/g
+const regexH6 = /[\#]{6} (.+)/g;
+const regexH5 = /[\#]{5} (.+)/g;
+const regexH4 = /[\#]{4} (.+)/g;
+const regexH3 = /[\#]{3} (.+)/g;
+const regexH2 = /[\#]{2} (.+)/g;
+const regexH1 = /[\#]{1} (.+)/g;
+const regexLi = /\- (.+)/g;
+const regexB = /[\*]{2}(.+)[\*]{2}/g;
+const regexB2 = /[\_]{2}(.+)[\_]{2}/g;
+const regexI = /[\_]{1}(.+)[\_]{1}/g;
+const regexI2 = /[\*]{1}(.+)[\*]{1}/g;
+const regexCode = /[\`](.+)[\`]/g;
+const regexCode2 = /[\`]{3}(.+)[\`]{3}/g;
+const regexLink = /\[(.+)\]\((.+)\)/g
+
+function parse(markdown) {
+  return markdown
+    .replace(regexBlockquote, "<blockquote><p>$1</p></blockquote>")
+    .replace(regexH6, "<h6>$1</h6>")
+    .replace(regexH5, "<h5>$1</h5>")
+    .replace(regexH4, "<h4>$1</h4>")
+    .replace(regexH3, "<h3>$1</h3>")
+    .replace(regexH2, "<h2>$1</h2>")
+    .replace(regexH1, "<h1>$1</h1>")
+    .replace(regexLi, "<li>$1</li>")
+    .replace(regexB, "<b>$1</b>")
+    .replace(regexB2, "<b>$1</b>")
+    .replace(regexI, "<i>$1</i>")
+    .replace(regexI2, "<i>$1</i>")
+    .replace(regexCode2, "<code>$1</code>")
+    .replace(regexCode, "<code>$1</code>")
+    .replace(regexImg, "<img src='$2' alt='$1'/>")
+    .replace(regexLink, "<a href='$2'>$1</a>")
+
+}
+
+
+export default parse;
---
+++ test/index.js
@@ -0,0 +1,30 @@
+import { test, expected } from "./test.js";
+import parse from "../src/index.js";
+
+const tests = [
+  expected(
+    "Test Blockquote",
+    parse(">hola"),
+    "<blockquote><p>hola</p></blockquote>",
+  ),
+  expected("Test h6", parse("###### Hola"), "<h6>Hola</h6>"),
+  expected("Test h5", parse("##### Hola"), "<h5>Hola</h5>"),
+  expected("Test h4", parse("#### Hola"), "<h4>Hola</h4>"),
+  expected("Test h3", parse("### Hola"), "<h3>Hola</h3>"),
+  expected("Test h2", parse("## Hola"), "<h2>Hola</h2>"),
+  expected("Test h1", parse("# Hola"), "<h1>Hola</h1>"),
+  expected("Test Li", parse("- hola"), "<li>hola</li>"),
+  expected("Test B", parse("**Hola**"), "<b>Hola</b>"),
+  expected("Test B2", parse("__Hola__"), "<b>Hola</b>"),
+  expected("Test I", parse("_Hola_"), "<i>Hola</i>"),
+  expected("Test I2", parse("*Hola*"), "<i>Hola</i>"),
+  expected("Test Code", parse("`Hola`"), "<code>Hola</code>"),
+  expected("Test Code2", parse("```Hola```"), "<code>Hola</code>"),
+  expected("Test Img", parse("![Hola](hola.js)"), "<img src='hola.js' alt='Hola'/>"),
+  expected("Test Link", parse("[Hola](hola.js)"), "<a href='hola.js'>Hola</a>"),
+
+];
+
+console.log(tests);
+
+test(tests);
